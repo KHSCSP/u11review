92f5fed3cd714933d1aca2601de2966da39c328f
scneeman <30477959+scneeman@users.noreply.github.com>
2026-01-18 10:37:47 -0600

    Add files via upload

---
+++ review.py
@@ -0,0 +1,19 @@
+'''
+Skills you will need
+
+---strings & lists---
+get item from a specific spot
+get slice from beginning to some spot
+get slice from some place to end
+loop through each item (using 'enhanced' loop AND indexed loop)
+use .count()
+use .index()
+
+---lists---
+create list of ints using list comprehension
+convert list of strings to list of ints
+
+---other algorithms---
+create list of tuples: (item, count(item))
+
+'''
