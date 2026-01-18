8f7ee48a357c52dadc6c51280cf3bc452b0eb97c
heyscottn <heyscottn@gmail.com>
2026-01-18 10:59:19 -0600

    mm

--- review.py
+++ review.py
@@ -17,3 +17,66 @@ convert list of strings to list of ints
 create list of tuples: (item, count(item))
 
 '''
+
+
+# TODO open, read, close
+
+
+
+# TODO convert all to lower case
+# print a small slice to check
+
+
+
+
+# TODO 'cleanse' the data
+# replace all:
+# "\n", ".", "!", ",", "?", ":"
+
+
+
+
+# TODO create a list of strings (hint: split at space)
+
+
+
+# TODO print the first and last item in your list
+
+
+
+# TODO print the length of your list
+
+
+
+# TODO print a small slice at the beginning (approx 20 items)
+
+
+
+# TODO use a for item in list loop
+# to print a small slice of your list (approx 20 items)
+
+
+
+# TODO use a for i in range loop
+# to print a small part of your list (approx 20 items)
+
+
+
+# TODO find the first occurence of 'state'
+# hint: .index()
+
+
+
+# TODO find how many times the word 'state' appears
+# hint: .count()
+
+
+
+
+
+# TODO count how many times each letter of the alphabet appears
+# make a list of tuples where each tuple is (letter, count(letter))
+
+
+
+
